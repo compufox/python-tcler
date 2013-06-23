bdb461ef2b91eb67949cd89857dc7015d870e7f1
Zac <zactepps@gmail.com>
2013-06-23 19:30:08 -0400

    Added ability for backlog to the console

--- twitclient.py
+++ twitclient.py
@@ -41,6 +41,12 @@
 #               IDEAS: make ERR a list that contains the backlog of errors
 #                and/or messages. Make console display the backlog of
 #                errors whenever it gets started
+#
+# 0.9.5         All messages/errors are stored in ERR which is now a global 
+#                list. When the console is started the backlog of messages
+#                (stored in ERR) are vomited into the console. These backlog
+#                messages are greyed out, where as the new messages are 
+#                not greyed. 
 
 import twitter
 import threading
@@ -50,8 +56,11 @@ from Tkinter import *
 from os import path
 from urllib2 import URLError
 from time import sleep
-from time import ctime
+from time import localtime
+from platform import system
 
+global ERR
+ERR = list()
 
 global STREAM_UPDATE
 STREAM_UPDATE = True
@@ -65,6 +74,7 @@ TEXT = None
 global CON
 CON = None
 
+# a simple class that allows for a Toplevel widget to become active with the console log
 class conDialog(Toplevel):
         def __init__(self, parent, title):
                 self.top = Toplevel(parent)
@@ -81,15 +91,27 @@ class conDialog(Toplevel):
                 self.close = Button(self.top, text="Close", command=self.closeThisWindow)
                 self.close.pack(fill=X,expand=0)
                 
+                self.logger.tag_config('backlog', foreground="grey")
+                
+                for e in ERR:
+                        self.logger.insert(INSERT, e + "\n", 'backlog')
+                
+                self.placeText(getTime() + "- Console Started")
+                
                 self.logger.config(state=DISABLED)
 
         def closeThisWindow(self):
+                self.placeText(getTime() + "- Console closed")
+                
                 self.top.destroy()
                 global CON
                 CON = None
                 
         def placeText(self, errors):
+                errors += "\n"
+                
                 self.logger.config(state=NORMAL)
+                ERR.append(errors)
                 self.logger.insert(INSERT, errors)
                 self.logger.config(state=DISABLED)
 
@@ -107,10 +129,12 @@ class upThread (threading.Thread):
                         self.tweet_id = args
                 
         def run(self):
-#                if CON != None:
-#                        CON.placeText(self.name + " starting...\n")
-#                else:
-                print(self.name + " starting...")
+                if CON != None and not self.threadID < 2:
+                        CON.placeText(getTime() + "- " + self.name + "-" + str(self.threadID) + " starting...")
+                elif self.threadID < 2:
+                        ERR.append(getTime() + "- " + self.name + "-" + str(self.threadID) + " starting...")
+                else:
+                        print(self.name + " starting...")
                 if self.name == "update":
                         if self.tweet_id != 1:
                                 update(0, self.tweet_id)
@@ -120,10 +144,12 @@ class upThread (threading.Thread):
                         numbers(self.entry)
                 elif self.name == "post":
                         post()
-#                if CON != None:
-#                        CON.placeText(self.name + " exiting...\n")
-#                else:
-                print(self.name + " exiting...")
+                if CON != None and not self.threadID < 2:
+                        CON.placeText(getTime() + "- " +self.name + "-" + str(self.threadID) + " exiting...")
+                elif CON != None and self.threadID < 2:
+                        CON.placeText(getTime() + "- " +self.name + "-" + str(self.threadID) + " exiting...")
+                else:
+                        print(self.name + " exiting...")
         def getName(self):
                 return(self.name)
         
@@ -131,7 +157,7 @@ class upThread (threading.Thread):
 # updates the text in the text widget with supplied tuple(?) of
 #  statuses. 
 #
-#  DO NOT CHANGE
+#  DO NOT CHANGE, unless you have a good reason (which I always have a good reason)
 def updateDisplay(status):
         text.config(state=NORMAL)
         
@@ -153,12 +179,12 @@ def updateDisplay(status):
                                         text.insert(counter, word, hyper.add(openLink, word))
                                 else:
                                         text.insert(counter, word + " ")
-#                                text.insert(counter+len(s.text),"\n")
+                                
                         text.insert(counter, newStat, "a")
                                 
                 except TclError:
                         if CON != None:
-                                CON.placeText(ctime() + "- A Tcl Character error occured, the offending tweet wasn't displayed\n")
+                                CON.placeText(getTime() + "- A Tcl Character error occured, the offending tweet wasn't displayed")
                         else:
                                 print "A Tcl Character error occured, the offending tweet wasn't displayed"
         
@@ -181,7 +207,7 @@ def openLink(link):
 # shows the console
 def showConsole():
         if CON != None:
-                CON.placeText(ctime() + "Console already shown\n")
+                CON.placeText(getTime() + "- Console already shown")
         else:
                 global CON
                 CON = console = conDialog(root, "Console")
@@ -200,16 +226,21 @@ def numbers(entry):
                 TEXT.set(140 - len(entry.get()))
                 sleep(.1)
 
-# quits the threads
+def getTime():
+        return str(localtime().tm_hour) + ":" + str(localtime().tm_min) + ":" + str(localtime().tm_sec) + " "
+
+# quits the threads and destroys the widgets
 def quit(thread):
-        global STREAM_UPDATE
-        STREAM_UPDATE = False
-        thread.join()
         
         if CON != None:
-                CON.top.destroy()
+                CON.closeThisWindow()
         
-        sys.exit()
+        global STREAM_UPDATE
+        STREAM_UPDATE = False
+        
+        thread.join()
+        
+        root.destroy()
 
 # starts a thread to run the post function. (Made this just so the GUI 
 #   doesn't unattractivly freeze up
@@ -232,11 +263,6 @@ def update(shot, last_id):
                                 if len(STATUSES) != 0:
                                         last_id = STATUSES[0].id
                                 
-#                                if counter == 15 or counter > 15:
-#                                        STATUSES = ()
-#                                        counter = 0
-#                                        sleep(60*14)
-                                
                                 if len(STATUSES) > 0 and LAST_ID != last_id:
                                         global LAST_ID
                                         LAST_ID = last_id
@@ -252,7 +278,7 @@ def update(shot, last_id):
                                 counter += 1
                         except twitter.TwitterError:
                                 if CON != None:
-                                        CON.placeText(ctime() + "- There was a Twitter problem getting the tweets\n")
+                                        CON.placeText(getTime() + "- There was a Twitter problem getting the tweets")
                                 else:
                                         print "There was a Twitter problem getting the tweet"
                                 for i in range(18):
@@ -264,7 +290,7 @@ def update(shot, last_id):
                                         break
                         except URLError:
                                 if CON != None:
-                                        CON.placeText(ctime() + "- There was a network issue when getting the tweets\n")
+                                        CON.placeText(getTime() + "- There was a network issue when getting the tweets")
                                 else:
                                         print "There was a network issue when getting the tweets"
                                 for i in range(18):
@@ -286,17 +312,19 @@ def update(shot, last_id):
                                 updateDisplay(STATUSES)
                 except twitter.TwitterError:
                         if CON != None:
-                                CON.placeText(ctime() + "- There was a Twitter problem getting the tweets\n")
+                                CON.placeText(getTime() + "- There was a Twitter problem getting the tweets")
                         else:
                                 print "There was a Twitter problem getting the tweet"
                 except URLError:
                         if CON != None:
-                                CON.placeText(ctime() + "- There was a network issue when getting the tweets\n")
+                                CON.placeText(getTime() + "- There was a network issue when getting the tweets")
                         else:
                                 print "There was a network issue when getting the tweets"
 
 ASS_KEY = None
 ASS_SECRET = None
+
+err = None
 
 from platform import system
 
@@ -310,7 +338,7 @@ else:
         if not path.exists(path.expanduser('~/.tcler')):
                 import get_access_token
         
-
+# gets the user's creds and splits the key and secret up
 red = open(path.expanduser('~/.tcler'), 'r').read().split('\n')
 ASS_KEY = red[0]
 ASS_SECRET = red[1]
@@ -321,9 +349,6 @@ api = twitter.Api(consumer_key='qJwaqOuIuvZKlxwF4izCw',
 		  access_token_key=ASS_KEY,
 		  access_token_secret=ASS_SECRET)
 
-# no errors! yet
-ERR = None
-
 # no statuses either
 STATUSES = None
 
@@ -331,13 +356,18 @@ STATUSES = None
 #  or a twitter error
 try:
         STATUSES = api.GetHomeTimeline()
-        
-#        global LAST_ID
-#        LAST_ID = STATUSES[0].id
 except URLError:
-        ERR = "There was a problem opening the network connection. Please ensure that your computer is online."
+        if err == None:
+                err = list()
+        
+        ERR.append(getTime() + "- There was a problem opening the network connection. Please ensure that your computer is online.")
+        err.append(ERR[len(ERR)-1])
 except twitter.TwitterError:
-        ERR = "Your timeline could not be retrieved at this time.\nPlease try again later."
+        if err == None:
+                err = list()
+        
+        ERR.append(getTime() + "- Your timeline could not be retrieved at this time.\nPlease try again later.")
+        err.append(ERR[len(ERR)-1])
 
 # tries to start a thread that just constantly runs the update function
 #  (see the update function docs for more info)
@@ -346,7 +376,11 @@ try:
         
         UPDATE_THREAD.start()
 except:
-        print(ctime() + "- Streaming update thread failed to start. Updating will have to be done manually.")
+        if err == None:
+                err = list()
+        
+        ERR.append(getTime() + "- Streaming update thread failed to start. Updating will have to be done manually.")
+        err.append(ERR[len(ERR)-1])
 
 # 
 # STARTS SETTING UP TK GUI STUFF
@@ -376,12 +410,6 @@ Label(root, textvariable=TEXT).pack(side=RIGHT)
 entry = Entry(root)
 entry.pack(fill=X, expand=1 ,side=RIGHT)
 
-#update_button = Button(root, text='Update', command=oneShotUpdate)
-#update_button.pack(side=RIGHT)
-
-#quit_button = Button(root, text='Quit', command=lambda: quit(UPDATE_THREAD))
-#quit_button.pack(side=LEFT)
-
 scroll.config(command=text.yview)
 
 menu = Menu(root)
@@ -390,9 +418,18 @@ menu.add_command(label="Console", command=showConsole)
 menu.add_command(label="Quit", command=lambda: quit(UPDATE_THREAD))
 root.config(menu=menu)
 
-if ERR != None:
+try:
+        NUMBER_THREAD = upThread(1, "numbers", entry)
+        NUMBER_THREAD.start()
+except:
+        if err == None:
+                err = list()
+        
+        ERR.append(getTime() + "- Starting the thread for numbers failed. Character counting will not be operational")
+        err.append(ERR[len(ERR)-1])
+if err != None:
         text.config(state=NORMAL)
-        text.insert(1.0, ERR)
+        text.insert(1.0, "Something has gone wrong, please check the console")
 
 #
 # END OF TK GUI STUFF
@@ -400,10 +437,7 @@ if ERR != None:
 
 # tries to start a thread that will keep the character count
 #  see the docs on the numbers function
-try:
-        NUMBER_THREAD = upThread(1, "numbers", entry)
-        NUMBER_THREAD.start()
-except:
-        print(ctime() + "- Starting the thread for numbers failed. Character counting will not be operational")
+
+ERR.append(getTime() + "- Main window started")
 
 root.mainloop()
