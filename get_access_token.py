f732889217be6df99a71a66a60912117d08d35d5
= <=>
2013-06-24 11:47:37 -0400

    Hopefully made get_access 'better'

--- get_access_token.py
+++ get_access_token.py
@@ -70,31 +70,26 @@ ACCESS_TOKEN_URL  = 'https://api.twitter.com/oauth/access_token'
 AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
 SIGNIN_URL        = 'https://api.twitter.com/oauth/authenticate'
 
-root = Tkinter.Tk()
-
-consumer_key    = 'qJwaqOuIuvZKlxwF4izCw'
-consumer_secret = '53dJ9tHJ77tAE8ywZIEU60JYPyoRU9jY2v0d58nI8'
-
-signature_method_hmac_sha1 = oauth.SignatureMethod_HMAC_SHA1()
-oauth_consumer             = oauth.Consumer(key=consumer_key, secret=consumer_secret)
-oauth_client               = oauth.Client(oauth_consumer)
-
-
-print 'Requesting temp token from Twitter'
-
-resp, content = oauth_client.request(REQUEST_TOKEN_URL, 'GET')
-
-if resp['status'] != '200':
-  print 'Invalid respond from Twitter requesting temp token: %s' % resp['status']
-else:
-  request_token = dict(parse_qsl(content))
-
-#  print ''
-#  print 'Please visit this Twitter page and retrieve the pincode to be used'
-#  print 'in the next step to obtaining an Authentication Token:'
-#  print ''
-#  print '%s?oauth_token=%s' % (AUTHORIZATION_URL, request_token['oauth_token'])
-#  print ''
+def startLogin():
+  
+  root = Tkinter.Tk()
+  
+  consumer_key    = 'qJwaqOuIuvZKlxwF4izCw'
+  consumer_secret = '53dJ9tHJ77tAE8ywZIEU60JYPyoRU9jY2v0d58nI8'
+  
+  signature_method_hmac_sha1 = oauth.SignatureMethod_HMAC_SHA1()
+  oauth_consumer             = oauth.Consumer(key=consumer_key, secret=consumer_secret)
+  oauth_client               = oauth.Client(oauth_consumer)
+  
+  
+  print 'Requesting temp token from Twitter'
+  
+  resp, content = oauth_client.request(REQUEST_TOKEN_URL, 'GET')
+  
+  if resp['status'] != '200':
+    print 'Invalid respond from Twitter requesting temp token: %s' % resp['status']
+  else:
+    request_token = dict(parse_qsl(content))
   
   label = Tkinter.Text(root)
   label.insert(1.0, 'If your browser didn\'t open please visit this Twitter page and retrive the pincode to be entered in below:\n' )
@@ -114,24 +109,6 @@ else:
   button = Tkinter.Button(root,text='Submit', command=getInfo)
   button.pack(side=Tkinter.RIGHT)
   
-
-
-#  print ''
-#  print 'Generating and signing request for an access token'
-#  print ''
-
-#  oauth_client  = oauth.Client(oauth_consumer, token)
-#  resp, content = oauth_client.request(ACCESS_TOKEN_URL, method='POST', body='oauth_callback=oob&oauth_verifier=%s' % pincode)
-#  access_token  = dict(parse_qsl(content))
-
-#  if resp['status'] != '200':
-#    print 'The request for a Token did not succeed: %s' % resp['status']
-#    print access_token
-#  else:
-#    print 'Your Twitter Access Token key: %s' % access_token['oauth_token']
-#    print '          Access Token secret: %s' % access_token['oauth_token_secret']
-#    print ''
-
-click()
-
-root.mainloop()
+  click()
+  
+  root.mainloop()
