c804f94494e2abf9eaa4e47c47cb7dfd5e0a53f1
Zac <zactepps@gmail.com>
2013-06-25 06:51:44 -0400

    py2exe setup script

---
+++ win_setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+import py2exe
+import sys
+
+if len(sys.argv) == 1:
+    sys.argv.append("py2exe")
+    sys.argv.append("-q")
+
+class Target:
+    def __init__(self, **kw):
+        self.__dict__.update(kw)
+        self.version = "0.9.5"
+        self.company_name = ""
+        self.copyright = "2013 theZacAttacks"
+        self.name = "Python Tcler"
+
+twitclient = Target(
+    description = "A simple Twitter client written in Python",
+    script = "twitclient.py")
+
+setup(
+    options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
+    zipfile = None,
+    windows = [twitclient],
+    )
